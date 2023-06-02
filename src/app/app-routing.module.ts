2224d22b571284553a4a0d510d3a39b17d455c79
abc <vaibhavdandage@gmail.com>
2023-06-02 21:53:52 +0530

    sc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"", redirectTo:"login",pathMatch:"full"},
+{path:"login",component:LoginComponent},
+{path:"contactus",component:ContactusComponent},
+{path:"aboutus",component:AboutusComponent},
+{path:"home",component:HomeComponent,
+  children:[
+    {path:"products", component:ProductsComponent}]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ContactusComponent,
+    AboutusComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
